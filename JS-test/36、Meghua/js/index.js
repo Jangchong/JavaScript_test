90c29bb594c5d822062974c0e35f4befba287e91
jiangchong <jiangchongluck@163.com>
2017-02-20 23:04:49 +0800

    delete

--- JS-test/36、Meghua/js/index.js
+++
@@ -1,7 +0,0 @@
-//$(function(){
-//	$('#body').fullpage({
-////		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
-////		anchors: ['#body', '#about', '#services', '#services'],
-////		menu: '#navigation'
-//	});
-//});
